0d9a2ad7f23d4cbd7171b2fc25116334d9061039
CKenny27 <106782235+CKenny27@users.noreply.github.com>
2023-03-30 09:43:16 -0500

    Se actualizo el login

--- lib/presentacion/screens/login/login.dart
+++ lib/presentacion/screens/login/login.dart
@@ -11,11 +11,24 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    // double height = MediaQuery.of(context).size.height;
+    // double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            
+            Expanded(
+                child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.blue[900],
+            )),
+            Expanded(child: Container())
           ],
         ),
       ),
